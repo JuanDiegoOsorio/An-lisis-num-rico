45966ef53d227c80983703fb796c6b0a8ae1e23e
JuanDiegoOsorio <41376489+JuanDiegoOsorio@users.noreply.github.com>
2018-07-27 08:43:50 -0500

    Update main.cpp

--- BiseccionEjercicio/main.cpp
+++ BiseccionEjercicio/main.cpp
@@ -10,11 +10,10 @@ int main()
 
     double sup, inf;
     int particiones;
-    cout<<"Ingrese el límite superior y el inferior: ";
+    cout<<"Ingrese el limite superior y el inferior: "<<endl;
     cin>>sup>>inf;
-    cout<<"\nIngrese el número de particicones";
+    cout<<"Ingrese el numero de particicones: "<<endl;
     cin>>particiones;
-    
     cout<<"\nEl valor es: "<< nseccion(sup,inf,particiones,0);
 
     return 0;
@@ -22,21 +21,25 @@ int main()
 
 double nseccion(double plot1, double plot2, int particion, int iteraciones){
     
-    //como comprobar que f(a) y f(b) es < que 0
+    //como comprobar que f(a) y f(b) < 0 y para cual funcion
+    // Funcion = x^3
     
     iteraciones++;
-    double value1 = (plot2-plot1)/pow(2,particion);
     
-    if (plot1*value1==0){
+    cout<<"\n Iteracion #"<<iteraciones<<": El error es= "<<plot2-plot1;
+    
+    double value1 = (plot2+plot1)/pow(2,particion);
+    
+    if (pow(plot1,3)*pow(value1,3)==0){
         return value1;
     }
     
-    if (plot1*value1<0){
+    if (pow(plot1,3)*pow(value1,3)<0){
         plot2 = value1;
         return nseccion(plot1,plot2, particion, iteraciones);
     }
 
-    if (plot1*value1>0){
+    if (pow(plot1,3)*pow(value1,3)>0){
         plot1 = value1;
         return nseccion(plot1,plot2, particion, iteraciones);
     }
