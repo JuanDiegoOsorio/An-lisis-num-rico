06d3bc95a7941982a3bab790e71e28202f97f0fa
JuanDiegoOsorio <41376489+JuanDiegoOsorio@users.noreply.github.com>
2018-07-27 07:55:34 -0500

    Update main.cpp

--- BiseccionEjercicio/main.cpp
+++ BiseccionEjercicio/main.cpp
@@ -1,43 +1,42 @@
 #include <iostream>
 
 using namespace std;
-
-double triseccion(double, double);
+#include <math.h>
+double nseccion(double, double, int,int);
 
 
 int main()
 {
 
+    double sup, inf;
+    int particiones;
+    cout<<"Ingrese el límite superior y el inferior: ";
+    cin>>sup>>inf;
+    cout<<"\nIngrese el número de particicones";
+    cin>>particiones;
+    
+    cout<<"\nEl valor es: "<< nseccion(sup,inf,particiones,0);
+
     return 0;
 }
 
-double triseccion(double plot1, double plot2){
-
-    double value1 = (plot1+plot2)/3;
-    double value2 = (2*(plot1+plot2))/3;
-
+double nseccion(double plot1, double plot2, int particion, int iteraciones){
+    
+    //como comprobar que f(a) y f(b) es < que 0
+    
+    iteraciones++;
+    double value1 = (plot2-plot1)/pow(2,particion);
+    
     if (plot1*value1==0){
         return value1;
     }
-    if (value1*value2==0){
-        return value2;
-    }
-    if (value2*plot2==0){
-        return plot2;
-    }
-
+    
     if (plot1*value1<0){
         plot2 = value1;
-        return triseccion(plot1,plot2);
-    }
-    if (value1*value2<0){
-        plot1 = value1;
-        plot2 = value2;
-        return triseccion(plot1,plot2);
-    }
-    if (value2*plot2<0){
-        plot1 = value2;
-        return triseccion(plot1,plot2);
+        return nseccion(plot1,plot2, particion, iteraciones);
     }
 
-}
+    if (plot1*value1>0){
+        plot1 = value1;
+        return nseccion(plot1,plot2, particion, iteraciones);
+    }
